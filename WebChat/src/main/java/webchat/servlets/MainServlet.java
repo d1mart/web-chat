a8f3f3fa5d1f11b4e102a13138545d9c140a3f2e
d1mart <dimmart13@gmail.com>
2015-06-03 00:27:31 +0300

    add1

---
+++ WebChat/src/main/java/webchat/servlets/MainServlet.java
@@ -0,0 +1,107 @@
+package webchat.servlets;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPathExpressionException;
+
+
+import webchat.model.Message;
+import org.apache.log4j.BasicConfigurator;
+import org.json.simple.JSONObject;
+
+import org.json.simple.parser.ParseException;
+import org.xml.sax.SAXException;
+import webchat.storage.XMLHistoryUtil;
+import webchat.util.MessageUtil;
+import webchat.util.ServletUtil;
+
+
+@WebServlet(urlPatterns = "/chat")
+public class MainServlet extends HttpServlet {
+    //private static Logger logger = Logger.getLogger(MainServlet.class.getName());
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy hh:mm");
+        String now = format.format(date.getTime());
+
+        String data = ServletUtil.getMessageBody(req);
+        System.out.println(now + ": " + data);
+
+        try {
+            JSONObject json = MessageUtil.stringToJson(data);
+            Message message = MessageUtil.jsonToMessage(json);
+            XMLHistoryUtil.addData(message);
+            resp.setStatus(HttpServletResponse.SC_OK);
+
+        } catch (ParseException e) {
+            System.out.println("badRequest");
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String token = req.getParameter(MessageUtil.TOKEN);
+        try {
+            if (token != null && !"".equals(token)) {
+                int index = MessageUtil.getIndex(token);
+
+                String messages;
+                messages = formResponse(index);
+                PrintWriter out = resp.getWriter();
+                out.print(messages);
+                out.flush();
+            } else {
+                resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "'token' parameter needed");
+            }
+        } catch (Exception e) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        try {
+            loadHistory();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("init");
+    }
+
+
+    private String formResponse(int index) throws SAXException, IOException, ParserConfigurationException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put(MessageUtil.MESSAGES, XMLHistoryUtil.getSubMessagesByIndex(index));
+        jsonObject.put(MessageUtil.TOKEN, MessageUtil.getToken(XMLHistoryUtil.getStorageSize()));
+        return jsonObject.toJSONString();
+    }
+
+    private void loadHistory() throws SAXException, IOException, ParserConfigurationException, TransformerException {
+        if (!XMLHistoryUtil.doesStorageExist()) {
+            XMLHistoryUtil.createStorage();
+        }
+    }
+}
---
+++ WebChat/src/main/java/webchat/util/ServletUtil.java
@@ -0,0 +1,28 @@
+package webchat.util;
+
+import webchat.model.Message;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ * Created by Pavel on 31.05.2015.
+ */
+public class ServletUtil {
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String UTF_8 = "UTF-8";
+
+    private ServletUtil() {
+    }
+
+    public static String getMessageBody(HttpServletRequest request) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = request.getReader();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+        }
+        return sb.toString();
+    }
+}
